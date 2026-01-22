d0e6a0d748ec6bdea0b2dd01c245fe65437038ce
seongjongju99 <seongjongju99@gmail.com>
2026-01-22 21:45:56 +0900

    인사말 애니메이션 재 수정

--- src/features/gsap/hook/useGsap.jsx
+++ src/features/gsap/hook/useGsap.jsx
@@ -83,7 +83,7 @@ const useGsap = () => {
                         y:0, opacity: 1, duration: 0.7, ease: 'power2.out',
                         scrollTrigger:{
                             trigger: saying.current,
-                            start:"top 60%",
+                            start:"top 50%",
                             toggleActions:"play none none none",
                         }
                     }
